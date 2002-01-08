d307b0c088d59ee978061ecc9fcc88c644c04ee5
Achim G"adke <achimgaedke@users.sourceforge.net>
2002-01-08 20:35:20 +0000

    added more scientific constants...

--- src/constmodule.c
+++ src/constmodule.c
@@ -17,11 +17,9 @@ static void add_constants(PyObject* m)
   PyModule_AddObject(m,"posinf",PyFloat_FromDouble(GSL_POSINF));
   PyModule_AddObject(m,"neginf",PyFloat_FromDouble(GSL_NEGINF));
   PyModule_AddObject(m,"nan",PyFloat_FromDouble(GSL_NAN));
-
   PyModule_AddObject(m,"e",PyFloat_FromDouble(M_E));
   PyModule_AddObject(m,"log2e",PyFloat_FromDouble(M_LOG2E));
   PyModule_AddObject(m,"log10e",PyFloat_FromDouble(M_LOG10E));
-
   PyModule_AddObject(m,"sqrt2",PyFloat_FromDouble(M_SQRT2));
   PyModule_AddObject(m,"sqrt1_2",PyFloat_FromDouble(M_SQRT1_2));
   PyModule_AddObject(m,"sqrt3",PyFloat_FromDouble(M_SQRT3));
@@ -36,6 +34,7 @@ static void add_constants(PyObject* m)
   PyModule_AddObject(m,"ln2",PyFloat_FromDouble(M_LN2));
   PyModule_AddObject(m,"lnpi",PyFloat_FromDouble(M_LNPI));
   PyModule_AddObject(m,"euler",PyFloat_FromDouble(M_EULER));
+# include "scientific_constants.c"
 }
 
 
