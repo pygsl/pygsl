7fe6b8cc4bae51dd05ec39663382ad01377328f3
Achim G"adke <achimgaedke@users.sourceforge.net>
2002-01-23 17:54:43 +0000

    leading edge development

--- tools/constants_tool.py
+++ tools/constants_tool.py
@@ -1,7 +1,7 @@
 from extract_tool import *
 
 output=file("scientific_constants.c","w")
-gsl_constants=constant_collector("/opt/gsl-1.0")
+gsl_constants=constant_collector("/opt/gsl-cvs")
 constant_list=gsl_constants.get_all_constants()
 for c in constant_list:
     output.write(c.make_module_definition()+";\n")
