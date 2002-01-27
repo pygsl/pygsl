e9c332abf6a5967718bda78f1e3a7516bc0de992
Achim G"adke <achimgaedke@users.sourceforge.net>
2002-01-27 17:07:38 +0000

    several changes

---
+++ src/const_num_array.c
@@ -0,0 +1,2 @@
+{"fine_structure",GSL_CONST_NUM_FINE_STRUCTURE,"1"},
+{"avogadro",GSL_CONST_NUM_AVOGADRO,"1 / mol"},
--- src/constmodule.c
+++ src/constmodule.c
@@ -15,22 +15,45 @@ static PyMethodDef constMethods[] = {
   {NULL,     NULL}        /* Sentinel */
 };
 
-static void add_math_constants(PyObject* m)
+typedef struct {
+  char* name;
+  double value;
+  char* doc;
+} constConstants;
+
+constConstants m_array[]={
+#include "const_m_array.c"
+{NULL,0,NULL}
+};
+
+constConstants mks_array[]={
+# include "const_mks_array.c"
+  {NULL,0,NULL}
+};
+
+constConstants cgs_array[]={
+# include "const_cgs_array.c"
+  {NULL,0,NULL}
+};
+
+constConstants num_array[]={
+# include "const_num_array.c"
+  {NULL,0,NULL}
+};
+
+
+
+static void
+add_constants(constConstants* constants, PyObject* m)
 {
-#include "const_add_math.c"
-}
-
-static void add_num_constants(PyObject* m)
-{
-#include "const_add_num.c"
-}
-
-static void add_mks_constants(PyObject *m){
-#include "const_add_mks.c"
-}
-
-static void add_cgs_constants(PyObject *m){
-#include "const_add_cgs.c"
+  int i=0;
+  while (constants[i].name!=NULL) {
+    PyObject* floatObject=PyFloat_FromDouble(constants[i].value);
+    if (floatObject==NULL) break;
+    PyModule_AddObject(m,constants[i].name,floatObject);
+    /* Py_DECREF(floatObject); */
+    i++;
+  }
 }
 
 DL_EXPORT(void) initconst(void)
@@ -38,24 +61,22 @@ DL_EXPORT(void) initconst(void)
   PyObject* const_module = Py_InitModule("const", constMethods);
   PyObject* mks_module   = PyImport_AddModule("pygsl.const.mks");
   PyObject* cgs_module   = PyImport_AddModule("pygsl.const.cgs");
-  PyObject* math_module  = PyImport_AddModule("pygsl.const.math");
+  PyObject* math_module  = PyImport_AddModule("pygsl.const.m");
   PyObject* num_module   = PyImport_AddModule("pygsl.const.num");
 
   /* distribute constants on modules */
-  add_math_constants(const_module);
-  add_num_constants(const_module);
-  add_mks_constants(const_module);
-  add_mks_constants(mks_module);
-  add_cgs_constants(cgs_module);
-  add_num_constants(num_module);
-  add_math_constants(math_module);
+  add_constants(m_array  , const_module);
+  add_constants(num_array, const_module);
+  add_constants(mks_array, const_module);
+  add_constants(mks_array, mks_module);
+  add_constants(cgs_array, cgs_module);
+  add_constants(num_array, num_module);
+  add_constants(m_array  , math_module);
 
-  /* add them to to the root module */
-  PyModule_AddObject(const_module,"cgs",cgs_module);
-  PyModule_AddObject(const_module,"mks",mks_module);
-  PyModule_AddObject(const_module,"math",math_module);
-  PyModule_AddObject(const_module,"num",num_module);
-
+  PyModule_AddObject(const_module, "m",   math_module);
+  PyModule_AddObject(const_module, "cgs", cgs_module);
+  PyModule_AddObject(const_module, "mks", mks_module);
+  PyModule_AddObject(const_module, "num", num_module);
   return;
 }
 
