96008b69f47163b7f3c7dd8e9a5c8a7a917845f1
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-02-27 17:41:44 +0000

    initial version

---
+++ pygsl/fit.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# Author : Pierre Schnizer <pierre@itp.tu-graz.ac.at>
+#  Date  : January 2003
+"""
+   The functions described in this section can be used to perform
+least-squares fits to a straight line model, Y = c_0 + c_1 X.  For
+weighted data the best-fit is found by minimizing the weighted sum of
+squared residuals, \chi^2,
+
+     \chi^2 = \sum_i w_i (y_i - (c_0 + c_1 x_i))^2
+
+for the parameters c_0, c_1.  For unweighted data the sum is computed
+with w_i = 1.
+"""
+
+import _callback
+
+def linear(x, y):
+    """
+     This function computes the best-fit linear regression coefficients
+     (C0,C1) of the model Y = c_0 + c_1 X for the datasets (X, Y), two
+     vectors of length N with strides XSTRIDE and YSTRIDE.  The
+     variance-covariance matrix for the parameters (C0, C1) is
+     estimated from the scatter of the points around the best-fit line
+     and returned via the parameters (COV00, COV01, COV11).  The sum of
+     squares of the residuals from the best-fit line is returned in
+     SUMSQ.
+    """
+    return _callback.gsl_fit_linear(x,y)
+
+def wlinear(x, y, w):
+    """
+    This function computes the best-fit linear regression coefficients
+     (C0,C1) of the model Y = c_0 + c_1 X for the weighted datasets (X,
+     Y), two vectors of length N with strides XSTRIDE and YSTRIDE.  The
+     vector W, of length N and stride WSTRIDE, specifies the weight of
+     each datapoint. The weight is the reciprocal of the variance for
+     each datapoint in Y.
+
+     The covariance matrix for the parameters (C0, C1) is estimated
+     from weighted data and returned via the parameters (COV00, COV01,
+     COV11).  The weighted sum of squares of the residuals from the
+     best-fit line, \chi^2, is returned in CHISQ.
+    """
+    return _callback.gsl_fit_wlinear(x, y, w)
+
+def est(x, c0, c1, c00, c01, c10):
+    """
+    This function uses the best-fit linear regression coefficients
+     C0,C1 and their estimated covariance COV00,COV01,COV11 to compute
+     the fitted function Y and its standard deviation Y_ERR for the
+     model Y = c_0 + c_1 X at the point X.
+    """
+    return _callback.gsl_fit_linear_est(x, c0, c1, c00, c01, c10)
+
+def mul(x,y):
+    """
+    This function computes the best-fit linear regression coefficient
+     C1 of the model Y = c_1 X for the datasets (X, Y), two vectors of
+     length N with strides XSTRIDE and YSTRIDE.  The variance of the
+     parameter C1 is estimated from the scatter of the points around
+     the best-fit line and returned via the parameter COV11.  The sum
+     of squares of the residuals from the best-fit line is returned in
+     SUMSQ.
+    """
+    return _callback.gsl_fit_mul(x,y)
+
+def wmul(x, y, w):
+    """
+    This function computes the best-fit linear regression coefficient
+     C1 of the model Y = c_1 X for the weighted datasets (X, Y), two
+     vectors of length N with strides XSTRIDE and YSTRIDE.  The vector
+     W, of length N and stride WSTRIDE, specifies the weight of each
+     datapoint. The weight is the reciprocal of the variance for each
+     datapoint in Y.
+
+     The variance of the parameter C1 is estimated from the weighted
+     data and returned via the parameters COV11.  The weighted sum of
+     squares of the residuals from the best-fit line, \chi^2, is
+     returned in CHISQ.
+    """
+    return _callback.gsl_fit_wmul(x, y, w)
+
+def mul_est(x, c1, c11):
+    """
+    This function uses the best-fit linear regression coefficient C1
+    and its estimated covariance COV11 to compute the fitted function
+    Y and its standard deviation Y_ERR for the model Y = c_1 X at the
+    point X.
+    """
+    return _callback.gsl_fit_mul_est(x, c1, c11)
+
+# def gsl_fit_poly(x, w, y):
+#     return _callback.gsl_fit_poly(x, w, y)
+# 
+# def gsl_fit_fns(A, w, y):
+#     return _callback.gsl_fit_fns(A, w, y)
+# 
+# def gsl_fit_linear_nd(m, y, w):
+#     return _callback.gsl_fit_linear_nd(m, y, w)
---
+++ pygsl/multifit.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+"""
+   The functions described in this section perform least-squares fits
+to a general linear model, y = X c where y is a vector of n
+observations, X is an n by p matrix of predictor variables, and c are
+the p unknown best-fit parameters, which are to be estimated.
+
+   The best-fit is found by minimizing the weighted sums of squared
+residuals, \chi^2,
+
+     \chi^2 = (y - X c)^T W (y - X c)
+
+with respect to the parameters c. The weights are specified by the
+diagonal elements of the n by n matrix W.  For unweighted data W is
+replaced by the identity matrix.
+
+   This formulation can be used for fits to any number of functions
+and/or variables by preparing the n-by-p matrix X appropriately.  For
+example, to fit to a p-th order polynomial in X, use the following
+matrix,
+
+     X_{ij} = x_i^j
+
+where the index i runs over the observations and the index j runs from
+0 to p-1.
+
+   To fit to a set of p sinusoidal functions with fixed frequencies
+\omega_1, \omega_2, ..., \omega_p, use,
+
+     X_{ij} = sin(\omega_j x_i)
+
+To fit to p independent variables x_1, x_2, ..., x_p, use,
+
+     X_{ij} = x_j(i)
+
+where x_j(i) is the i-th value of the predictor variable x_j.
+
+   The functions described in this section are declared in the header
+file `gsl_multifit.h'.
+
+   The solution of the general linear least-squares system requires an
+additional working space for intermediate results, such as the singular
+value decomposition of the matrix X.
+
+"""
+# Author : Pierre Schnizer <pierre@itp.tu-graz.ac.at>
+#  Date  : January 2003
+
+import _callback
+from _generic_solver import _workspace
+
+class linear_workspace(_workspace):
+    """
+    Class handling the workspace required for fitting.
+
+    """
+    _alloc = _callback.gsl_multifit_linear_alloc
+    _free  = _callback.gsl_multifit_linear_free
+    
+    def __init__(self, n, p):
+        """
+        Allocates a workspace for fitting a model to N
+        observations using P parameters.
+
+        input: N, P
+            N ... number of samples
+            P ... number of parameters
+        """
+        self._ptr = None
+        assert(self._alloc != None)
+        assert(self._free != None)
+        self._ptr = self._alloc(n, p)
+
+def linear(X, y, work):
+    """
+    This function computes the best-fit parameters C of the model y =
+    X c for the observations Y and the matrix of predictor variables
+    X.  The variance-covariance matrix of the model parameters COV is
+    estimated from the scatter of the observations about the best-fit.
+    The sum of squares of the residuals from the best-fit, \chi^2, is
+    returned in CHISQ.
+    
+    The best-fit is found by singular value decomposition of the matrix
+    X using the preallocated workspace provided in WORK. The modified
+    Golub-Reinsch SVD algorithm is used, with column scaling to
+    improve the accuracy of the singular values. Any components which
+    have zero singular value (to machine precision) are discarded from
+    the fit.
+    """
+    return _callback.gsl_multifit_linear(X, y, work._ptr)
+
+def wlinear(X, y, w, work):
+    """
+    This function computes the best-fit parameters C of the model y =
+    X c for the observations Y and the matrix of predictor variables
+    X.  The covariance matrix of the model parameters COV is estimated
+    from the weighted data.  The weighted sum of squares of the
+    residuals from the best-fit, \chi^2, is returned in CHISQ.
+
+    The best-fit is found by singular value decomposition of the matrix
+    X using the preallocated workspace provided in WORK. Any
+    components which have zero singular value (to machine precision)
+    are discarded from the fit.
+    """
+    return _callback.gsl_multifit_wlinear(X, y, w, work._ptr)
