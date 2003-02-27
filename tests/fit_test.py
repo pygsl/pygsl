e96710d40a2386bf6dd60515877502cba00e4369
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-02-27 17:25:26 +0000

    intial version

---
+++ tests/fit_test.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# Author : Pierre Schnizer 
+import unittest
+import random
+import pygsl
+from pygsl import fit
+from pygsl import multifit
+import Numeric
+
+
+class FitTest(unittest.TestCase):
+    _eps = 1e-8
+    def setUp(self):
+        self.a = 1000.1
+        self.b = 100
+        self.x = Numeric.arange(100)
+        self.y = self.a + self.b * self.x
+        self.w = Numeric.ones(100) * 1000000
+
+
+    def test_0_linear(self):
+        tmp, c0, c1, c00, c01, c10, sumsq =  fit.linear(self.x, self.y)
+        assert(Numeric.absolute(c0 - self.a)<self._eps)
+        assert(Numeric.absolute(c1 - self.b)<self._eps)
+        y, yerr =  fit.est(1, c0, c1, c00, c01, c10)[1:]
+        assert(Numeric.absolute(y - (self.a+self.b*1)) < self._eps)
+        assert(Numeric.absolute(yerr) < self._eps)
+        
+
+        
+    def test_1_wlinear(self):
+        tmp, c0, c1, c00, c01, c10, chisq = fit.wlinear(self.x, self.w, self.y)
+        assert(Numeric.absolute(c0 - self.a)<self._eps)
+        assert(Numeric.absolute(c1 - self.b)<self._eps)
+        y, yerr =  fit.est(1, c0, c1, c00, c01, c10)[1:]
+        assert(Numeric.absolute(y - (self.a+self.b*1)) < self._eps)
+
+class FitMulTest(unittest.TestCase):
+    _eps = 1e-8
+    def setUp(self):
+        self.a = 0.0
+        self.b = 100
+        self.x = Numeric.arange(100)
+        self.y = self.a + self.b * self.x
+        self.w = Numeric.ones(100) * 1000000
+
+
+    def test_0_linear(self):
+        tmp, c1, c11, sumsq =  fit.mul(self.x, self.y)
+        assert(tmp==0)
+        assert(Numeric.absolute(c1 - self.b)<self._eps)
+        y, yerr =  fit.mul_est(1, c1, c11)[1:]
+        assert(Numeric.absolute(y - (self.a+self.b*1)) < self._eps)
+        assert(Numeric.absolute(yerr) < self._eps)
+        
+
+        
+    def test_1_wlinear(self):
+        tmp, c1,  c11, chisq = fit.wmul(self.x, self.w, self.y)
+        assert(Numeric.absolute(c1 - self.b)<self._eps)
+        y, yerr =  fit.mul_est(1, c1, c11)[1:]
+        assert(Numeric.absolute(y - (self.a+self.b*1)) < self._eps)
+
+
+        
+class MultiFitTest(unittest.TestCase):
+    _eps = 1e-10
+    def setUp(self):
+        self.dim   = 100
+        self.param = 2
+        self.a = 1000.1
+        self.b = 100        
+        self.x = Numeric.arange(self.dim)
+        x = self.x
+        self.y = self.a + self.b * self.x
+        self.w = Numeric.ones((self.dim,))
+        self.ws = multifit.linear_workspace(self.dim, self.param)
+        self.X = Numeric.transpose(Numeric.array((Numeric.ones(self.dim,), x)))
+
+    
+    def test_linear(self):
+        flag, c, cov, chisq = multifit.linear(self.X, self.y, self.ws)
+        assert(flag == 0)
+        assert(Numeric.absolute(c[0] - self.a) < self._eps)
+        assert(Numeric.absolute(c[1] - self.b) < self._eps)
+
+    def test_wlinear(self):
+        flag, c, cov, chisq = multifit.wlinear(self.X, self.w, self.y, self.ws)
+        assert(flag == 0)
+        assert(Numeric.absolute(c[0] - self.a) < self._eps)
+        assert(Numeric.absolute(c[1] - self.b) < self._eps)
+
+        
+if __name__ == '__main__':
+    unittest.main()
