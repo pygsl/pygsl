62c893b9feb5094a976d9b511c3fca9dec4965a6
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-02-27 17:21:10 +0000

    Change to the new interface of odeiv

--- examples/odeiv.py
+++ examples/odeiv.py
@@ -12,14 +12,14 @@ import Numeric
 import pygsl
 from pygsl import odeiv
 
-mu = 10.0
-def func(t, y):
+
+def func(t, y, mu):
     f = Numeric.zeros((2,), Numeric.Float) * 1.0
     f[0] = y[1]
     f[1] = -y[0] - mu * y[1] * (y[0] ** 2 -1);
     return f
 
-def jac(t, y):
+def jac(t, y, mu):
     dfdy = Numeric.ones((2,2), Numeric.Float) 
     dfdy[0, 0] = 0.0
     dfdy[0, 1] = 1.0
@@ -30,6 +30,7 @@ def jac(t, y):
     
 def run():
 
+    mu = 10.0
     dimension = 2
     # The different possible steppers for the function
     # Comment your favourite one out to test it.
@@ -44,7 +45,7 @@ def run():
     #stepper = odeiv.step_gear2
     stepper = odeiv.step_bsimp
     
-    step = stepper(dimension, func,jac)
+    step = stepper(dimension, func,jac, mu)
     # All above steppers exept the odeiv.step_bsimp (Buerlisch - Stoer Improved
     # method can calculate without an jacobian ...
     # step = stepper(dimension, func)
@@ -54,15 +55,19 @@ def run():
     
     print  "# Using stepper %s with order %d" %(step.name(), step.order())
     print  "# Using Control ", control.name()
-
+    print "# %5s %9s %9s  %9s " % ("iter", "t", "y[0]", "y[1]")
     h = 1
     t = 0.0
     t1 = 50.0
     y = (1.0, 0.0)
-    while t<t1:
+    for i in range(1000):
+        if t >= t1:
+            break
         t, h, y = evolve.apply(t, t1, h, y)
-        print t, y[0], y[1]
+        print "  %5d % 10.6f % 10.6f  % 10.6f " %(i, t, y[0], y[1])
 
+    else:
+        raise ValueError, "Maximum number of steps exceeded!"
     
 if __name__ == '__main__':
     run()
