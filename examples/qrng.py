f1ed0216318bfb3abd36173e9763cb072edb51ca
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-06-20 17:40:19 +0000

    Initial version

---
+++ examples/qrng.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+The pythonic version of the qrng example found in the gsl reference document.
+"""
+import Numeric
+import pygsl.qrng as qrng
+
+def run():
+    q = qrng.sobol(2)
+    for i in q(1024):
+        print i[0], i[1]
+
+if __name__ == '__main__':
+    run()
