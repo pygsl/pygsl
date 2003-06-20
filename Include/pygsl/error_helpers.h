d70043c5a88eee1ba40eb1944093da4a37746d8e
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-06-20 17:58:12 +0000

    Added function to a traceback frame

--- Include/pygsl/error_helpers.h
+++ Include/pygsl/error_helpers.h
@@ -3,9 +3,29 @@
 #include <pygsl/utils.h>
 #include <Python.h>
 
+/*
+ * Consider the passed integer as a error flag. 
+ * Negative values mean something like go one with the iteration. These are 
+ * converted to an python integer. Positive values flag a problem. These are 
+ * converted to python exceptions.
+ */
 #define PyGSL_ERROR_FLAG_TO_PYINT(flag)                                     \
 (((long) flag <= 0) && (!PyErr_Occurred())) ? PyInt_FromLong((long) flag) : \
                      PyGSL_error_flag_to_pyint((long) (flag))
+
 PyObject * 
 PyGSL_error_flag_to_pyint(long flag);
 #endif /* PyGSL_ERROR_HELPER_H  */
+
+/*
+ * Add a Python trace back frame to the python interpreter.
+ * Input :
+ *     module   ... the module. Pass NULL if not known.
+ *     filename ... The filename to list in the stack frame. Pass NULL if not 
+ *                  known.
+ *     funcname ... The function name to list in the stack frame. Pass NULL if
+ *                  not known.
+ *     lineno   ... The Linenumber where the error occurred.
+ */
+void 
+PyGSL_add_traceback(PyObject *module, char *filename, char *funcname, int lineno);
--- Lib/error_helpers.c
+++ Lib/error_helpers.c
@@ -1,6 +1,8 @@
 #include <pygsl/error_helpers.h>
 #include <pygsl/utils.h>
 #include <gsl/gsl_errno.h>
+#include <compile.h>
+#include <frameobject.h>
 
 PyObject * 
 PyGSL_error_flag_to_pyint(long flag)
@@ -19,4 +21,86 @@ PyGSL_error_flag_to_pyint(long flag)
      }
      result = PyInt_FromLong((long) flag);
      return result;
+}
+
+void 
+PyGSL_add_traceback(PyObject *module, char *filename, char *funcname, int lineno)
+{
+     PyObject *py_srcfile = NULL, *py_funcname = NULL, *py_globals = 0,
+	  *empty_tuple = 0,  *empty_string = 0;
+     PyCodeObject *py_code = NULL;
+     PyFrameObject *py_frame = NULL;
+     
+     FUNC_MESS_BEGIN();
+
+     if(filename == NULL)
+	  filename = "file ???";
+     py_srcfile = PyString_FromString(filename);
+     if (py_srcfile == NULL) 
+	  goto fail;
+
+     if(funcname == NULL)
+	  funcname = "function ???";
+
+     py_funcname = PyString_FromString(funcname);
+     if (py_funcname == NULL) 
+	  goto fail;
+
+     /* Use the module if provided */
+     if(module == NULL){
+	  py_globals = PyDict_New();
+     } else {
+	  py_globals = PyModule_GetDict(module);
+     } 
+     if (py_globals == NULL) 
+	  goto fail;
+
+     empty_tuple = PyTuple_New(0);
+     if (empty_tuple == NULL) 
+	  goto fail;
+
+     empty_string = PyString_FromString("");
+     if (empty_string == NULL) 
+	  goto fail;
+
+     py_code = PyCode_New(
+	  0,            /*int argcount,*/
+	  0,            /*int nlocals,*/
+	  0,            /*int stacksize,*/
+	  0,            /*int flags,*/
+	  empty_string, /*PyObject *code,*/
+	  empty_tuple,  /*PyObject *consts,*/
+	  empty_tuple,  /*PyObject *names,*/
+	  empty_tuple,  /*PyObject *varnames,*/
+	  empty_tuple,  /*PyObject *freevars,*/
+	  empty_tuple,  /*PyObject *cellvars,*/
+	  py_srcfile,   /*PyObject *filename,*/
+	  py_funcname,  /*PyObject *name,*/
+	  lineno,       /*int firstlineno,*/
+	  empty_string  /*PyObject *lnotab*/
+	  );
+     if (py_code == NULL) 
+	  goto fail;
+
+     py_frame = PyFrame_New(
+	  PyThreadState_Get(), /*PyThreadState *tstate,*/
+	  py_code,             /*PyCodeObject *code,*/
+	  py_globals,          /*PyObject *globals,*/
+	  0                    /*PyObject *locals*/
+	  );
+
+     if (py_frame == NULL) 
+	  goto fail;
+     py_frame->f_lineno = lineno;
+     PyTraceBack_Here(py_frame);
+
+     FUNC_MESS_END();
+ fail:
+     FUNC_MESS("Handling failure");
+     Py_XDECREF(py_srcfile);
+     Py_XDECREF(py_funcname);
+     Py_XDECREF(empty_tuple);
+     Py_XDECREF(empty_string);
+     Py_XDECREF(py_code);
+     Py_XDECREF(py_frame);
 }
