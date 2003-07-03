adc1858d66475526eed61a89c23fded2505397c9
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-07-03 08:55:44 +0000

    Added a new implementation of sf and rng

---
+++ setup_testing.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python2
+import sys
+import distutils
+from distutils.core import setup, Extension
+from gsl_Extension import gsl_Extension
+from distutils import sysconfig
+from common_objects import libpygsl
+
+exts = []
+rng=gsl_Extension("rng",
+                  ['testing/src/rngmodule_testing.c'],
+                  gsl_min_version=(1,),
+                  python_min_version=(2,0)
+                  )
+exts.append(rng)    
+sf=gsl_Extension("sf",
+                 ['testing/src/sfmodule_testing.c'],
+                 gsl_min_version=(1,),
+                 python_min_version=(2,0)
+                 )
+exts.append(sf)    
+setup (name = "pygsl",
+       version = "0.1b-testing",
+       description = "GNU Scientific Library Interface",
+       author = "Pierre Schnizer",
+       author_email = "schnizer@users.sourceforge.net",
+       url = "http://pygsl.sourceforge.net",
+       py_modules = [
+                     ],
+       ext_package = 'pygsl.testing',
+       ext_modules = exts,
+       libraries  = [libpygsl, ]
+       )
