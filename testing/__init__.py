539d2ef31a1ce661e729903f4a38a586273c93c9
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-07-03 08:40:17 +0000

    A point to collect and test new implementations of modules

---
+++ testing/__init__.py
@@ -0,0 +1,6 @@
+"""
+Here you find either new implemented modules or alternate implementations 
+of already modules. This directory is intended to have a second implementation
+beside the main implementation to have a discussion which implementation to 
+favor on the long run.
+"""
