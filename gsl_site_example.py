9acf72a13852d67e1148953613b312e2d4896112
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-07-25 18:01:04 +0000

    Put better examples

--- gsl_site_example.py
+++ gsl_site_example.py
@@ -1,12 +1,13 @@
 # Some systems do not provide gsl-config. So here the locations can be entered
 # by hand.
+import os
 
 # The path to the dir
-prefix = "/usr"
+prefix = os.path.join("F:\\", "Programs", "GSL", "gsl-1.3")
 
-cflags = "-I/usr/include"
+cflags = "-g -I" + os.path.join(prefix, "include")
 
-libs   = "-L/usr/lib -lgsl -lgslcblas -lm"
+libs   = "-L" + os.path.join(prefix, "lib")+ " -lgsl -lgslcblas -lm"
 
 # Enter the correct versions by hand..
 version = "1.3"
