c89bda20837e86d56319b9b34e35238962d0a056
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-12-17 08:03:02 +0000

    the error module is needed by  the error handler. So I import it to be sure it exists

--- pygsl/__init__.py
+++ pygsl/__init__.py
@@ -3,6 +3,9 @@
 # file: pygsl/__init__.py
 # $Id$
 
+# Central Module used by C callbacks. So a good idea to import it here to be
+# sure that  it exists from the very beginning!
+import pygsl.errors
 import pygsl.init
 
 version="0.0.5"
