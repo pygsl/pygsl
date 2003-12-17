5f89d947d5e2aeef9461e8c17840c3484c217133
Pierre Schnizer <schnizer@users.sourceforge.net>
2003-12-17 09:21:29 +0000

    Handles gsl1.3 and gsl1.4 incompabilites of cgs<-> cgsm and mks <-> mksa

--- tests/const_test.py
+++ tests/const_test.py
@@ -1,7 +1,19 @@
+import string
 import unittest
 import math
 import pygsl.const
 
+version = string.split(pygsl.compiled_gsl_version, '.')
+major_version = version[0]
+minor_version = version[1]
+
+if major_version == 1 and minor_version < 4:
+    import pygsl.const.cgs as cgs
+    import pygsl.const.mks as mks
+else:
+    import pygsl.const.cgsm as cgs
+    import pygsl.const.mksa as mks
+    
 class math_const_test(unittest.TestCase):
     """
     the mere existence of mathmatical constants and simple correlations are tested
@@ -19,16 +31,16 @@ class sci_const_test(unittest.TestCase):
     the mere existence of scientific constants and simple correlations are tested
     """
     def test_cgs_c(self):
-        my_c=pygsl.const.cgs.speed_of_light
+        my_c=cgs.speed_of_light
 
     def test_c(self):
         """
         is the factor 100 between meter and centimeter correct?
         """
-        self.failIf(pygsl.const.cgs.speed_of_light/pygsl.const.mks.speed_of_light!=100)
+        self.failIf(cgs.speed_of_light/mks.speed_of_light!=100)
 
     def test_default(self):
-        self.failIf(pygsl.const.cgs.speed_of_light/pygsl.const.speed_of_light!=100)
+        self.failIf(cgs.speed_of_light/pygsl.const.speed_of_light!=100)
 
 if __name__ == "__main__":
     unittest.main()
