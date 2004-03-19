6e4586e56dcf12556772c0349c7a6655e2704f56
Pierre Schnizer <schnizer@users.sourceforge.net>
2004-03-19 18:19:18 +0000

    SWIG Sync of movement of callback helpers to the callback module

--- swig_src/callback_wrap.c
+++ swig_src/callback_wrap.c
@@ -783,6 +783,7 @@ PyObject *pygsl_module_for_error_treatment = NULL;
 
 
 #include <pygsl/error_helpers.h>
+#include "function_helpers.c"
 
 
   gsl_function * gsl_function_init(gsl_function * STORE)
