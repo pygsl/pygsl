e49c1a6395620bccba6e02f50c2af7ba58ca59c8
Pierre Schnizer <schnizer@users.sourceforge.net>
2004-08-03 09:33:40 +0000

    Implementation of the deriv module.
    Starting with gsl1.5 the diff module was obsoleted. Instead the deriv module was introduced. The deriv module allows to specify a start step size.
    So there is not too much difference for the wrapper. Common functions have been placed into diff_deriv_common.c. The diff and deriv offer the modules for the two implementations.

--- src/derivmodule.c
+++ src/derivmodule.c
@@ -1,33 +1,17 @@
-/**
- * author: Jochen K"upper
- * created: April 2002
- * file: pygsl/src/diffmodule.c
- * $Id$
- *
- * December 2003: Changes by Pierre Schnizer <schnizer@users.sourceforge.net>
- *     Changed return value to nan, if an error occurs.
- *     Changed the three function into one, adding the function pointer which 
- *     diff to use.
- *     The wrapper now uses PyGSL_function_wrap_helper the common wrapper for 
- *     gsl function
- *
- *     Made all static variables local to the function. So now it should be 
- *     threadsave.
- *
- *     Unfortunatley gsl_function does not allow error reporting. Thus I use
- *     a jmp_buf to get back to the calling function.
- * " <- To Fix Emacs colouring
- */
-
-#define PyGSL_DIFF_MODULE
+#define PyGSL_DERIV_MODULE
 #include "diff_deriv_common.c"
 
-#define DIFF_FUNCTION(name)                                                  \
-static PyObject* diff_ ## name (PyObject *self, PyObject *args)              \
+
+
+
+
+
+#define DERIV_FUNCTION(name)                                                  \
+static PyObject* deriv_ ## name (PyObject *self, PyObject *args)              \
 {                                                                            \
      PyObject *tmp = NULL;                                                   \
      FUNC_MESS_BEGIN();                                                      \
-     tmp = PyGSL_diff_generic(self, args,  gsl_diff_ ## name);               \
+     tmp = PyGSL_diff_generic(self, args,  gsl_deriv_ ## name);              \
      if (tmp == NULL){                                                       \
 	  PyGSL_add_traceback(module, __FILE__, __FUNCTION__, __LINE__);     \
      }                                                                       \
@@ -35,29 +19,29 @@ static PyObject* diff_ ## name (PyObject *self, PyObject *args)              \
      return tmp;                                                             \
 }
 
-DIFF_FUNCTION(backward)
-DIFF_FUNCTION(forward)
-DIFF_FUNCTION(central)
+DERIV_FUNCTION(backward)
+DERIV_FUNCTION(forward)
+DERIV_FUNCTION(central)
 
 	
 
 /* module initialization */
 #define PyGSL_DIFF_USAGE  "\n See module doc string for function call description."
-static PyMethodDef diffMethods[] = {
-	{"backward", diff_backward, METH_VARARGS,
-	 "Computer derivative of |f| at |x| using backward differences." \
+static PyMethodDef derivMethods[] = {
+	{"backward", deriv_backward, METH_VARARGS,
+	 "Computer derivative of |f| at |x| with stepsize |H| using backward differences." \
 	 "Returns |value| and |abserr|." PyGSL_DIFF_USAGE},
-	{"central", diff_central, METH_VARARGS,
-	 "Computer derivative of |f| at |x| using central differences." \
+	{"central", deriv_central, METH_VARARGS,
+	 "Computer derivative of |f| at |x| with stepsize |H| using central differences." \
 	 "Returns |value| and |abserr|." PyGSL_DIFF_USAGE},
-	{"forward", diff_forward, METH_VARARGS,
-	 "Computer derivative of |f| at |x| using forward differences." \
+	{"forward", deriv_forward, METH_VARARGS,
+	 "Computer derivative of |f| at |x| with stepsize |H| using forward differences." \
 	 "Returns |value| and |abserr|."PyGSL_DIFF_USAGE },
 	{NULL, NULL} /* Sentinel */
 };
 
 
-static const char diff_module_doc[] = 
+static const char deriv_module_doc[] = 
 "Numerical differentation \n\
 \n\
 This module allows to differentiate functions numerically. It provides\n\
@@ -67,20 +51,21 @@ the following functions:\n\
          forward\n\
 \n\
 All have the same usage:\n\
-         func(callback, x, [args])\n\
+         func(callback, x, h, [args])\n\
               callback ... foo(x, args):\n\
                                ... some calculation here ...\n\
                                return y\n\
               x        ... the position where to differentate the callback\n\
+              h        ... initial step size used to calculate the optimal one\n\
               args     ... additional object to be passed to the function.\n\
                            It is optional. In this case None is passed as\n\
                            args to foo\n\
 ";
-DL_EXPORT(void) initdiff(void)
+DL_EXPORT(void) initderiv(void)
 {
 	PyObject *m = NULL, *dict = NULL, *item = NULL;
 
-	m = Py_InitModule("diff", diffMethods);
+	m = Py_InitModule("deriv", derivMethods);
 	init_pygsl();
         import_array();
 	if (m == NULL)
@@ -90,7 +75,7 @@ DL_EXPORT(void) initdiff(void)
 	if (dict == NULL)
 		return;
 	
-	if (!(item = PyString_FromString(diff_module_doc))){
+	if (!(item = PyString_FromString(deriv_module_doc))){
 		PyErr_SetString(PyExc_ImportError, 
 				"I could not generate module doc string!");
 		return;
---
+++ src/diff_deriv_common.c
@@ -0,0 +1,147 @@
+#if (PYGSL_GSL_MAJOR_VERSION == 1) && (PYGSL_GSL_MINOR_VERSION < 5)
+#define _PYGSL_HAS_DERIV 0
+#else 
+#define _PYGSL_HAS_DERIV 1
+#endif 
+
+#ifdef  _PYGSL_HAS_DERIV 
+#include <gsl/gsl_deriv.h>
+#endif
+
+#ifdef PyGSL_DERIV_MODULE
+#ifndef _PYGSL_HAS_DERIV
+#error "The deriv module was only introduced by GSL 1.5. You seem to compile against an older verion!"
+#endif
+#endif /* PyGSL_DERIV_MODULE */
+
+#include <gsl/gsl_diff.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_math.h>
+#include <Python.h>
+
+#include <pygsl/error_helpers.h>
+#include <pygsl/function_helpers.h>
+#include <setjmp.h>
+/* 
+ * callback functions
+ * - python function passed by user
+ * - actual C callback
+ * - GSL wrapper struct
+ */
+
+/* Used for traceback */
+static PyObject *module = NULL;
+
+typedef struct{
+	PyObject * callback;
+	PyObject * args;
+	jmp_buf  buffer;
+}pygsl_diff_args;
+
+
+static double 
+diff_callback(double x, void *p)
+{
+	double value;
+	int flag;
+	pygsl_diff_args *pargs = NULL;
+
+	pargs = (pygsl_diff_args *) p;
+
+	assert(pargs->callback);
+	assert(pargs->args);
+	flag = PyGSL_function_wrap_helper(x, &value, NULL, pargs->callback,
+					  pargs->args, __FUNCTION__);
+
+	if(GSL_SUCCESS != flag){
+		longjmp(pargs->buffer, flag);
+		return gsl_nan();
+	}
+	return value;
+}
+
+/* wrapper function */
+typedef int pygsl_deriv_func(const gsl_function *, double, double, double *, double *);
+typedef int pygsl_diff_func(const gsl_function *, double, double *, double *);
+
+static PyObject *
+PyGSL_diff_generic(PyObject *self, PyObject *args, 
+#ifndef PyGSL_DIFF_MODULE
+		   pygsl_deriv_func func
+#else 
+		   pygsl_diff_func func
+#endif
+)
+{
+	PyObject *result=NULL, *myargs=NULL;
+	PyObject *cb=NULL;
+
+	pygsl_diff_args pargs = {NULL, NULL};
+	/* Changed to compile using Sun's Compiler */
+	gsl_function diff_gsl_callback = {NULL, NULL};
+
+
+
+
+	double x, value, abserr;
+	int flag;
+#ifndef PyGSL_DIFF_MODULE
+	double h;
+	if(! PyArg_ParseTuple(args, "Odd|O", &cb, &x, &h, &myargs)){
+		return NULL;
+	}
+#else
+	if(! PyArg_ParseTuple(args, "Od|O", &cb, &x, &myargs)){
+		return NULL;
+	}
+#endif
+
+	/* Changed to compile using Sun's Compiler */
+	diff_gsl_callback.function = diff_callback;
+	diff_gsl_callback.params = (void *) &pargs;
+
+	if(! PyCallable_Check(cb)) {
+		PyErr_SetString(PyExc_TypeError, 
+				"The first parameter must be callable");
+		return NULL;
+	}
+	Py_INCREF(cb);               /* Add a reference to new callback */
+
+	pargs.callback = cb;        /* Remember new callback */
+
+
+	/* Did I get arguments? If so handle them */
+	if(NULL == myargs){
+		Py_INCREF(Py_None);
+		pargs.args= Py_None;
+	}else{
+		Py_INCREF(myargs);
+		pargs.args= myargs;
+	}
+
+	if((flag=setjmp(pargs.buffer)) == 0){
+		/* Jmp buffer set, call the function */
+#ifndef PyGSL_DIFF_MODULE
+	     flag = func(&diff_gsl_callback, x, h, &value, &abserr);
+#else
+	     flag = func(&diff_gsl_callback, x, &value, &abserr);	
+#endif
+	}else{
+		DEBUG_MESS(2, "CALLBACK called longjmp!");
+	}
+	
+	/* Arguments no longer used */
+	Py_DECREF(pargs.args);
+
+	/* Dispose of callback */
+	Py_DECREF(pargs.callback);
+
+ 
+	if(flag != GSL_SUCCESS){
+		PyGSL_ERROR_FLAG(flag);
+		return NULL;
+	}
+
+	result = Py_BuildValue("(dd)", value, abserr);
+	return result;
+}
