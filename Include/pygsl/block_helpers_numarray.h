6b7c788a7e78a5ec74d2dcbc06f45533341133f7
Pierre Schnizer <schnizer@users.sourceforge.net>
2005-01-04 12:15:37 +0000

    Added macros for numarray support

--- Include/pygsl/block_helpers_numarray.h
+++ Include/pygsl/block_helpers_numarray.h
@@ -4,7 +4,9 @@
 #ifndef _PyGSL_API_MODULE
 
 #endif /* _PyGSL_API_MODULE */
-
+#define import_array() import_libnumarray()
+#define PyArray_Return(obj) ((PyObject *) obj)
+#define PyArray_Check(obj)  NA_NumArrayCheck((obj))
 /*
  * No special macro conversions here, lets first just use the compiled functions.
  */
