faff249c6117ec0f8b17509e25ebd33623cdb228
Pierre Schnizer <schnizer@users.sourceforge.net>
2006-02-23 20:25:57 +0000

    Originaly in solver.h. Now internals separated from exported api <pygsl/solver.h>

---
+++ testing/src/solvers/solver_intern.h
@@ -0,0 +1,43 @@
+#ifndef _PYGSL_SOLVER_INTERN_H_
+#define _PYGSL_SOLVER_INTERN_H_ 1
+
+#include <pygsl/general_helpers.h>
+#include <pygsl/block_helpers.h>
+#include <pygsl/function_helpers.h>
+#include <setjmp.h>
+#include <gsl/gsl_math.h>
+
+#define _PyGSL_SOLVER_API_MODULE
+#include <pygsl/solver.h>
+#undef _PyGSL_SOLVER_API_MODULE
+static PyTypeObject PyGSL_solver_pytype;
+
+#define PyGSL_solver_check(ob) ((ob)->ob_type == &PyGSL_solver_pytype)
+#define _PyGSL_solver_n_init PyGSL_solver_n_init
+
+static PyGSL_solver *
+_PyGSL_solver_init(const struct _SolverMethods *methods, 
+		   const struct _GSLMethods* raw_methods);
+
+
+
+static int *
+PyGSL_solver_set_called(PyGSL_solver *self);
+
+#define PyGSL_SOLVER_SET_CALLED(ob) \
+        (((ob)->set_called == 1) ? GSL_SUCCESS: PyGSL_solver_set_called((ob)))
+
+PyGSL_API_EXTERN int
+PyGSL_solver_func_set(PyGSL_solver *self, PyObject *args, PyObject *f,
+		       PyObject *df, PyObject *fdf);
+
+
+#if 0
+PyGSL_SOLVER_STATIC  PyObject *
+_PyGSL_solver_np_init(PyObject *self, PyObject *args, const solver_alloc_struct * alloc);
+
+PyGSL_SOLVER_STATIC PyObject *
+_PyGSL_solver_i_vvdd(PyObject * self, PyObject * args, int_f_vvdd_t func);
+
+#endif
+#endif /* _PYGSL_SOLVER_INTERN_H_ */
