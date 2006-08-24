e68385f84ad52cd7c06e8cf98cedcd0cbaaaee33
Pierre Schnizer <schnizer@users.sourceforge.net>
2006-08-24 11:31:45 +0000

    The ufunc module provides now functions from comlex_math and sf

---
+++ pygsl/testing/complex.py
@@ -0,0 +1,15 @@
+import _ufuncs
+_token = "complex_"
+_tokl  = len(_token)
+
+for _name in dir(_ufuncs):
+    if _name[:_tokl] == _token:
+        _shortname = _name[_tokl:]
+        _cmd = "%s = _ufuncs.%s" % (_shortname, _name)
+        #printy cmd
+        exec(_cmd)
+del _token
+del _tokl
+del _shortname
+del _name
+del _cmd
