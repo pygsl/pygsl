3069c04972f7ffd17d69986c4caaaa9074c5f9d3
Pierre Schnizer <schnizer@users.sourceforge.net>
2006-12-07 19:48:24 +0000

    added myord_numpy

--- tests/array_check.py
+++ tests/array_check.py
@@ -2,11 +2,15 @@ import pygsl
 import pygsl._numobj
 nummodule = pygsl._numobj
 ArrayType = pygsl.ArrayType
-    
-get_typecode = pygsl.get_typecode
 
 def myord_numeric(obj):
     return ord(obj)
+
+def myord_numpy(obj):
+    if obj == "":
+        return 0
+    return ord(obj)
+
 
 def myord_default(obj):
     """
@@ -18,12 +22,16 @@ def myord_default(obj):
 def myorda_default(obj):
     return myord(obj)
 
+
 def myorda_numeric(obj):
     return myord(obj[0])
 
 if nummodule.nummodule == "Numeric":
     myord = myord_numeric
     myorda = myorda_numeric
+elif nummodule.nummodule == "numpy":
+    myord = myord_numpy
+    myorda = myorda_default
 else:
     myord = myord_default
     myorda = myorda_default
@@ -33,10 +41,11 @@ def array_check(array, arraytype=None, size=None):
     shape = None
     assert(type(array) == ArrayType)
     try:
-        if arraytype:
-            assert(get_typecode(array) == arraytype)
+        tmp = array.dtype
+        if arraytype != None:
+            assert(tmp == arraytype)
 
-        if size:
+        if size != None:
             shape = array.shape
             assert(len(shape) == len(size))
             for i in range(len(size)):
@@ -45,7 +54,8 @@ def array_check(array, arraytype=None, size=None):
         test = 1        
     finally:            
         if test == 0:
-            print "Got an array of type", type(array)
+            print 
+            print "Got an array of type", tmp,
             print "With type '%s' and shape '%s'" % (get_typecode(array), shape)
             print "Expected an array of ", ArrayType,
             print "With type '%s' and shape '%s'" % (arraytype, size)
