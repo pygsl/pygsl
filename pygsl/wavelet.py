f39d77ebc7fd83b6daafdb73552e5aa38ee302b3
Pierre Schnizer <schnizer@users.sourceforge.net>
2006-12-07 19:30:33 +0000

    shortend module doc string

--- pygsl/wavelet.py
+++ pygsl/wavelet.py
@@ -1,8 +1,5 @@
 """
-The wrapper for the wavelets are in the catchall for all the transfroms.
-
-They are gathered here to be 
-
+Wavelet transfroms as provided by GSL
 """
 from pygsl._transform import daubechies, haar, bspline
 from pygsl._transform import daubechies_centered, haar_centered, bspline_centered
