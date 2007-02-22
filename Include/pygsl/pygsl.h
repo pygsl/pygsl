d7847c122fc4665ed5550aaf5f3c370c2896f451
Pierre Schnizer <schnizer@users.sourceforge.net>
2007-02-22 19:16:13 +0000

    Initial version. Collection of all helper headers

---
+++ Include/pygsl/pygsl.h
@@ -0,0 +1,7 @@
+#ifndef _PYGSL_H_
+#define _PYGSL_H_ 1
+#include <pygsl/utils.h>
+#include <pygsl/error_helpers.h>
+#include <pygsl/general_helpers.h>
+#include <pygsl/block_helpers.h>
+#endif /* _PYGSL_H_ */
