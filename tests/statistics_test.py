8acfe8adcd7a96316f5e25dfb99762398cceb4f6
Pierre Schnizer <schnizer@users.sourceforge.net>
2008-02-09 16:35:42 +0000

    made numpy names default ones

--- tests/statistics_test.py
+++ tests/statistics_test.py
@@ -6,25 +6,31 @@
 #
 #
 import unittest
-from pygsl._numobj import *
+import  pygsl._numobj as numx
 try:
-    Int8
-except NameError:
-    Int8 = int8
+    Int8 = numx.int8
+except AttributeError:
+    Int8 = numx.Int8
 try:
-    Int32
-except NameError:
-    Int32 = int32
+    Int32 = numx.int32
+except AttributeError:
+    Int32  = numx.Int32
+
 
 try:
-    Int16
-except NameError:
-    Int16 = int16
+    Int = numx.int_
+except AttributeError:
+    Int = numx.Int
 
 try:
-    UInt8
-except NameError:
-    UInt8 = uint8
+    Int16 = numx.int16
+except AttributeError:
+    Int16 = numx.Int16
+
+try:
+    UInt8 = numx.Uint8
+except AttributeError:
+    UInt8 = numx.uint8
 
 from pygsl.statistics import *
 from pygsl.statistics import char as char
@@ -39,10 +45,10 @@ class statistics_test(unittest.TestCase):
     """Simple tests on statistics functions"""
 
     def test_wmean(self):        
-        self.failIf(wmean(array([1,1,1]), array([-1.,-3.,1.])) != -1.0)
-        self.failIf(wmean(array([1,1,1]),[1,2,3]) != 2)
-        data = array([1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
-        weight = ones(data.shape)
+        self.failIf(wmean(numx.array([1,1,1]), numx.array([-1.,-3.,1.])) != -1.0)
+        self.failIf(wmean(numx.array([1,1,1]),[1,2,3]) != 2)
+        data = numx.array([1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
+        weight = numx.ones(data.shape)
         self.failIf(wmean(weight,data) != 5.5)
         
         self.failIf(wmean(weight[::2],data[::2]) != 5.0)
@@ -50,13 +56,13 @@ class statistics_test(unittest.TestCase):
         self.failIf(wmean(weight[::-2],data[::-2]) != 6.0)
             
     def test_mean(self):
-        self.failIf(mean(array([-1.,-3.,1.])) != -1.0)
+        self.failIf(mean(numx.array([-1.,-3.,1.])) != -1.0)
         self.failIf(mean([1,2,3]) != 2)
         # test stride != 1
         # these are only valid tests when using NumPy,
         # since otherwise the sequence is trnsformed to an
         # contigous array anyway...
-        data = array([1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
+        data = numx.array([1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
         self.failIf(mean(data) != 5.5)
 
         self.failIf(mean(data[::2]) != 5.0)
@@ -65,75 +71,74 @@ class statistics_test(unittest.TestCase):
 
     def test_mean_char(self):
         tmp = (ord("1") + ord("2") + ord("3")) / 3
-        self.failIf(char.mean([1,2,3]) != tmp)
-        self.failIf(char.mean(array([1,2,3], Int8)) != tmp)
+        self.failIf(char.mean(numx.array([1,2,3], Int8)) != 2)
+        self.failIf(char.mean([1,2,3]) != 2)
     
     def test_mean_float(self):
-        self.failIf(float.mean(array([-1.,-3.,1.], 'f')) != -1)
+        self.failIf(float.mean(numx.array([-1.,-3.,1.], 'f')) != -1)
         self.failIf(float.mean([1.,2.,3.]) != 2)
     
     def test_mean_int(self):
-        self.failIf(int.mean(array([-1,-3,1], Int32)) != -1)
+        self.failIf(int.mean(numx.array([-1,-3,1], Int16)) != -1)
         self.failIf(int.mean([1,2,3]) != 2)
     
     def test_mean_long(self):
-        self.failIf(long.mean(array([-1,-3,1])) != -1)
+        self.failIf(long.mean(numx.array([-1,-3,1])) != -1)
         self.failIf(long.mean([1,2,3]) != 2)
         
     def test_mean_short(self):
-        self.failIf(short.mean([1,2,3]) != 2)
-        
-        self.failIf(short.mean(array([-1,-3,1], Int16)) != -1)
+        self.failIf(short.mean([1,2,3]) != 2)        
+        self.failIf(short.mean(numx.array([-1,-3,1], Int16)) != -1)
 
     def test_mean_uchar(self):
-        self.failIf(uchar.mean(array([1,2,3], UInt8)) != 2)
+        self.failIf(uchar.mean(numx.array([1,2,3], UInt8)) != 2)
         self.failIf(uchar.mean([1,2,3]) != 2)
         
     def test_sd(self):
-        self.failIf(sd(array([-1.,-3.,1.])) != 2.0)
+        self.failIf(sd(numx.array([-1.,-3.,1.])) != 2.0)
 
     def test_sd_long(self):
-        self.failIf(long.sd(array([-1,-3,1])) != 2)
+        self.failIf(long.sd(numx.array([-1,-3,1])) != 2)
 
     def test_sd_m(self):
-        self.failIf(sd_m(array([-1.,-3.,1.]),
-                         mean(array([-1.,-3.,1.]))) != 2.0)
+        self.failIf(sd_m(numx.array([-1.,-3.,1.]),
+                         mean(numx.array([-1.,-3.,1.]))) != 2.0)
         
     def test_sd_m_long(self):
-        self.failIf(long.sd_m(array([-1,-3,1]),
-                              long.mean(array([-1,-3,1]))) != 2)
+        self.failIf(long.sd_m(numx.array([-1,-3,1]),
+                              long.mean(numx.array([-1,-3,1]))) != 2)
 
     def test_variance(self):
-        self.failIf(variance(array([-1.,-3.,1.])) != 4.0)
+        self.failIf(variance(numx.array([-1.,-3.,1.])) != 4.0)
         return
 
     def test_variance_long(self):
-        self.failIf(long.variance(array([-1,-3,1])) != 4.0)
+        self.failIf(long.variance(numx.array([-1,-3,1])) != 4.0)
         return
     
     def test_variance_m(self):
-        self.failIf(variance_m(array([-1.,-3.,1.]),
-                               mean(array([-1.,-3.,1.]))) != 4.0)
+        self.failIf(variance_m(numx.array([-1.,-3.,1.]),
+                               mean(numx.array([-1.,-3.,1.]))) != 4.0)
         return
         
     def test_variance_m_long(self):
-        self.failIf(long.variance_m(array([-1,-3,1]),
-                                    long.mean(array([-1,-3,1]))) != 4)
+        self.failIf(long.variance_m(numx.array([-1,-3,1]),
+                                    long.mean(numx.array([-1,-3,1]))) != 4)
         return
 
 
     def test_min_long(self): 
-        self.failIf(long.min(array([1,2,3])) != 1)
+        self.failIf(long.min(numx.array([1,2,3])) != 1)
 
     def test_max_long(self):
-        self.failIf(long.max(array([1,2,3])) != 3)
+        self.failIf(long.max(numx.array([1,2,3])) != 3)
 
 
     def test_min_index(self): 
-        self.failIf(long.min_index(array([1,2,3])) != 0)
+        self.failIf(long.min_index(numx.array([1,2,3])) != 0)
 
     def test_max_index(self):
-        self.failIf(long.max_index(array([1,2,3])) != 2)
+        self.failIf(long.max_index(numx.array([1,2,3])) != 2)
 
 
 if __name__ == "__main__":
