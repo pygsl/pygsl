6dcce9a9ce4a324885f7b94f1de43a085d1581ee
Pierre Schnizer <schnizer@users.sourceforge.net>
2008-02-09 16:35:16 +0000

    modified array_check function

--- tests/array_check.py
+++ tests/array_check.py
@@ -2,6 +2,7 @@ import pygsl
 import pygsl._numobj
 nummodule = pygsl._numobj
 ArrayType = pygsl.ArrayType
+get_typecode = pygsl.get_typecode
 
 def myord_numeric(obj):
     return ord(obj)
@@ -36,20 +37,32 @@ else:
     myord = myord_default
     myorda = myorda_default
     
-def array_check(array, arraytype=None, size=None):
+def array_check(array, arraytype=None, thesize=None):
+    """
+    Check if the array is as expected
+    """
     test = 0
     shape = None
-    assert(type(array) == ArrayType)
+    #assert(type(array) == ArrayType)
     try:
         tmp = array.dtype
         if arraytype != None:
             assert(tmp == arraytype)
 
-        if size != None:
-            shape = array.shape
-            assert(len(shape) == len(size))
-            for i in range(len(size)):
-                assert(size[i] == shape[i])
+        if thesize != None:
+            myshape = array.shape
+            lsize = len(thesize)
+            assert(len(myshape) == lsize)
+            srange = list(range(lsize))
+            test = 0
+            try:
+                for myi in srange:
+                    assert(thesize[myi] == myshape[myi])
+                test = 1
+            finally:
+                if test == 0:
+                    print "lsize -->%s<-- type:%s srange -->%s<-- type:%s" %(lsize, type(lsize), srange, type(srange))
+                    
                 
         test = 1        
     finally:            
@@ -58,7 +71,7 @@ def array_check(array, arraytype=None, size=None):
             print "Got an array of type", tmp,
             print "With type '%s' and shape '%s'" % (get_typecode(array), shape)
             print "Expected an array of ", ArrayType,
-            print "With type '%s' and shape '%s'" % (arraytype, size)
+            print "With type '%s' and shape '%s'" % (arraytype, thesize)
 
 
     
