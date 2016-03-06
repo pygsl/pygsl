fbb49b035310e61709bd777c562de238f5716b71
Pierre Schnizer <schnizer@users.sourceforge.net>
2016-03-06 17:23:58 +0000

    Getting ready for GSL 2.1 or higher while retaining compability for GSL 1.x

--- Include/pygsl/pygsl_features.h
+++ Include/pygsl/pygsl_features.h
@@ -6,20 +6,25 @@
  * to GSL during the development.
  */
 
-#if PYGSL_GSL_MAJOR_VERSION < 1
+#if PYGSL_GSL_MAJOR_VERSION < 1 
 #error "PyGSL requires at least GSL 1.0"
 #endif 
 	  
 /*
  * currently the wrapper is only written for GSL 1.xx
+ *
+ * But testing with GSL 2.0
  */
 #if PYGSL_GSL_MAJOR_VERSION == 1
+#else
+#warning "Testing for version 2.?"
+#endif /* PYGSL_GSL_MAJOR_VERSION == 1 */
 
 
 /*
  * The NMSIMPLEX solver was added with version 1.3
  */
-#if PYGSL_GSL_MINOR_VERSION >= 3	  
+#if  (PYGSL_GSL_MAJOR_VERSION > 1) ||  (PYGSL_GSL_MAJOR_VERSION  == 1 && PYGSL_GSL_MINOR_VERSION >= 3)	
 #define _PYGSL_GSL_HAS_FMINIMIZER_NMSIMPLEX
 #endif
 
@@ -27,7 +32,7 @@
 /*
  * The deriv module was added with version 1.5
  */
-#if (PYGSL_GSL_MINOR_VERSION < 5)
+#if ((PYGSL_GSL_MAJOR_VERSION == 1) && (PYGSL_GSL_MINOR_VERSION < 5))
 #define _PYGSL_GSL_HAS_DERIV 0
 #else 
 #define _PYGSL_GSL_HAS_DERIV 1
@@ -37,14 +42,14 @@
 /*
  * The wavelet module
  */
-#if (PYGSL_GSL_MINOR_VERSION > 5)
+#if (PYGSL_GSL_MAJOR_VERSION > 1) ||  (PYGSL_GSL_MAJOR_VERSION  == 1 && (PYGSL_GSL_MINOR_VERSION > 5))
 #define _PYGSL_GSL_HAS_WAVELET 1
 #endif
 
 /*
  * When was KNUTHRAN 2002 really added?
  */
-#if  PYGSL_GSL_MINOR_VERSION > 8
+#if (PYGSL_GSL_MAJOR_VERSION > 1) || (PYGSL_GSL_MINOR_VERSION > 8)
 #define _PYGSL_GSL_HAS_RNG_KNUTHRAN2002 1
 #endif
 
@@ -54,18 +59,15 @@
  *
  * That was an on and off. Hopefully it is correct.
  */
-#if (PYGSL_GSL_MINOR_VERSION < 4) || (PYGSL_GSL_MINOR_VERSION > 5)
+#if (PYGSL_GSL_MAJOR_VERSION > 1) || (PYGSL_GSL_MINOR_VERSION < 4) || (PYGSL_GSL_MINOR_VERSION > 5)
 #define _PYGSL_GSL_HAS_CGS 1
 #define _PYGSL_GSL_HAS_MKS 1
 #endif 
 
-#if (PYGSL_GSL_MINOR_VERSION >= 4)
+#if (PYGSL_GSL_MAJOR_VERSION > 1) || (PYGSL_GSL_MINOR_VERSION >= 4)
 #define _PYGSL_GSL_HAS_CGSM 1
 #define _PYGSL_GSL_HAS_MKSA 1
 #endif
 
-#else
-#error "Should be version 1."
-#endif /* PYGSL_GSL_MAJOR_VERSION == 1 */
 
 #endif /* _PYGSL_FEATURES_H_ */
