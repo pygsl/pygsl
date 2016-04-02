d94207f25e42d6b940fb47bef56fbc3e95447680
Pierre Schnizer <schnizer@users.sourceforge.net>
2016-04-02 07:39:28 +0000

    Array used for evaluation

--- examples/interpolation2d.py
+++ examples/interpolation2d.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import pygsl
-from pygsl import interpolation2d
+from pygsl import interpolation2d, spline2d
 np = pygsl._numobj
 
 x_a = np.array([0,1])
 y_a = np.array([0,1])
-z_a = np.array([(1,2),
+z_a = np.array([(1,  2),
                 (1.5,2)])
 x_size = len(x_a)
 y_size = len(y_a)
@@ -20,14 +21,55 @@ assert(N > 1)
 X = np.arange(N) / (N - 1)
 Y = np.arange(N) / (N - 1)
 
-for x in X:
-    for y in Y:
-        test = 0
-        try:
-            z = intp.eval(x, y)
-            test = 1
-        finally:
-            if test == 0:
-                print("Failure evaluating: x = %f y = %f" % (x, y))
+# Example of evaluating the interpolation over a matrix ... but only evaluating
+# an array internally....
 
-        print("%f %f %f" %(x,y,z))
+# 
+t_shape = (len(X), len(Y))
+X_all = X[:,np.newaxis]
+Y_all = Y[np.newaxis,:]
+
+print("Using interpolation 2d")
+Z_all = intp.eval_array(X_all, Y_all)
+
+z = intp.eval(X[1], Y[1])
+        
+print("Using spline 2d")
+sp = spline2d.bilinear(x_size, y_size)
+sp.init(x_a, y_a, z_a)
+
+Z_all = sp.eval_array(X_all, Y_all)
+z = sp.eval(X[1], Y[1])
+
+xp = .5
+yp = .5
+
+msg = "sp x = %e, y = %e: f = %e, df/dx = %e df/dy = %e\n"
+msg += "    d^2f/dx^2 = %e d^2f/dxdy = %e d^2f/dydy = %e "
+print(msg %
+      (
+          xp, yp,
+          sp.eval(xp, yp),
+          sp.eval_deriv_x(xp, yp),
+          sp.eval_deriv_y(xp, yp),
+          sp.eval_deriv_xx(xp, yp),
+          sp.eval_deriv_xy(xp, yp),
+          sp.eval_deriv_yy(xp, yp)
+      )
+)
+
+print(msg %
+      (
+          xp, yp,
+          sp.eval_e(xp, yp),
+          sp.eval_deriv_x_e(xp, yp),
+          sp.eval_deriv_y_e(xp, yp),
+          sp.eval_deriv_xx_e(xp, yp),
+          sp.eval_deriv_xy_e(xp, yp),
+          sp.eval_deriv_yy_e(xp, yp)
+      )
+)
+
+print("intp: extract", intp.eval_extrap(1.5, .5))
+print("intp: extract", intp.eval_e_extrap(1.5, .5))
+
