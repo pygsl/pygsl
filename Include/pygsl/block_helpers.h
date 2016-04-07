22d936700d851884982cbbe2df3a16da4c7cc651
Pierre Schnizer <schnizer@users.sourceforge.net>
2016-04-07 15:46:20 +0000

    macro for continuous io arrays

--- Include/pygsl/block_helpers.h
+++ Include/pygsl/block_helpers.h
@@ -139,6 +139,7 @@ enum PyGSL_Array_Flags {
    PyGSL_BUILD_ARRAY_INFO(array_flag, NPY_DOUBLE, sizeof(double), argnum) 
 #define PyGSL_DARRAY_CINPUT(argnum)  PyGSL_DARRAY_INFO(PyGSL_CONTIGUOUS     | PyGSL_INPUT_ARRAY, argnum) 
 #define PyGSL_DARRAY_INPUT(argnum)   PyGSL_DARRAY_INFO(PyGSL_NON_CONTIGUOUS | PyGSL_INPUT_ARRAY, argnum) 
+#define PyGSL_DARRAY_CIO(argnum)     PyGSL_DARRAY_INFO(PyGSL_CONTIGUOUS     | PyGSL_IO_ARRAY,    argnum) 
 
 /*
  * PyGSL_New_Array:
