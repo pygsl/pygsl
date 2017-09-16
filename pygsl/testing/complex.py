2eb1beaf8c9d2610fe9fd525ce874c608a18360c
Pierre Schnizer <schnizer@users.sourceforge.net>
2017-09-17 00:37:40 +0200

    fixed debug print

--- pygsl/testing/complex.py
+++ pygsl/testing/complex.py
@@ -1,12 +1,14 @@
+from __future__ import print_function
 from . import _ufuncs
 _token = "complex_"
 _tokl  = len(_token)
 
 for _name in dir(_ufuncs):
+    #print("name", _name)
     if _name[:_tokl] == _token:
         _shortname = _name[_tokl:]
         _cmd = "%s = _ufuncs.%s" % (_shortname, _name)
-        #printy cmd
+        #print(_cmd)
         exec(_cmd)
 del _token
 del _tokl
