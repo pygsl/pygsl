a8fceabfad84add401c9110a5b707c961c584cfd
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2017-09-23 20:52:04 +0200

    added handling TEST_SF2; refactored code to minimise duplication

--- testing/tests/sf_test_basis.py
+++ testing/tests/sf_test_basis.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import print_function
+from __future__ import print_function, division
 import pickle
 import unittest
 import numpy as np
@@ -16,7 +16,11 @@ import pygsl.ieee
 import pygsl.testing.sf as sf
 import pygsl._numobj as np
 import pygsl.gslwrap
-from pygsl import errno
+from pygsl import errno, errors
+
+class TestNoFuncError(errors.gsl_Error):
+    "Called function was not specified!"
+    pass
 
 gsl_isnan = pygsl.gslwrap.gsl_isnan
 gsl_fcmp = pygsl.gslwrap.gsl_fcmp
@@ -39,9 +43,27 @@ TEST_TOL6 = (1048576.0*GSL_DBL_EPSILON)
 TEST_SQRT_TOL0 = (2.0*GSL_SQRT_DBL_EPSILON)
 TEST_SNGL =  (1.0e-06)
 
+
+_tol_dic = {
+    "TEST_TOL0": TEST_TOL0, 
+    "TEST_TOL1": TEST_TOL1, 
+    "TEST_TOL2": TEST_TOL2, 
+    "TEST_TOL3": TEST_TOL3, 
+    "TEST_TOL4": TEST_TOL4, 
+    "TEST_TOL5": TEST_TOL5, 
+    "TEST_TOL6": TEST_TOL6, 
+    "TEST_SNGL": TEST_SNGL, 
+    "TEST_SQRT_TOL0": TEST_SQRT_TOL0    
+    }
+
 class _test_sf_automatic(unittest.TestCase):
+    """Base class for all tests
+    """
     _func = None
+
     def _GetArgs(self, args):
+        """Convert string to arguments
+        """
         types = self._func.types
         a_type = types[-1]
         inargs, outargs = a_type.split('->')
@@ -77,18 +99,58 @@ class _test_sf_automatic(unittest.TestCase):
             #    raise ValueError(msg %(type_code, cnt, inargs))
 
             elif type(arg) == type(""):
-                arg = eval(arg)                
+                arg = eval(arg)
             
             l.append(arg)
             cnt += 1
         args = tuple(l)    
         return args
 
-    def _EvalFunc(self, args):
-        return result
+    #def _Check_Func(self):
+    #    pass
 
-    def _Check_Func_With_Error(self, orig_args, result, tolerance):
-        args = self._GetArgs(orig_args)
+    def _ConvertError(self, err):
+        """Converts standard values used by GSL
+        """
+        if type(err) == type(""):
+                err = eval(err)
+        return err
+
+    def _ConvertTolerance(self, tol):
+        
+        try:
+            tol = float(tol)
+            return tol
+        except ValueError as ve:
+            pass
+        
+        if type(tol) == type(""):
+            try:
+                tol = _tol_dic[tol]
+                return tol
+            except KeyError as ke:
+                pass
+
+        tol = eval(tol)
+        return tol
+        
+    
+    
+    def _EvalFunc(self, *args, **kws):
+        """Evaluate the function to test
+        
+        The method:
+          * ensures that the method is not None
+          * If a  TypeError is raised during execution, the argument values and
+            the function name will be added
+
+        Make sure func member is defined. Call it with args and kwsx
+        """
+        
+        if self._func is None:
+           fmt = "%s:%s self_func is None Thus I do not know which one to evaluate!"
+           msg = fmt %(__file__, self.__class__.__name__)           
+           raise ValueError(msg)
 
         def gen_arg_desc(arg):
             t_type = type(arg)
@@ -97,32 +159,66 @@ class _test_sf_automatic(unittest.TestCase):
         
         msg = tuple(map(gen_arg_desc, args))
         msg = "args: " + " ".join(msg)
+           
         func_name = self._func.__name__
-
-        # If the full func doc is needed for error analysis
-        #func_doc = self._func.__doc__
-        #func_doc = map(lambda x: x.strip(), func_doc.split("\n"))
-        #func_doc = ";".join(func_doc)
-        
         msg += " func: '%s' '%s'" % (func_name, self._func.types)
 
-        pygsl.ieee.env_setup()
         try:
-            t = self._func(*args)
+           result = self._func(*args, **kws)
         except TypeError as t_err:
             t_err.args += (msg,)
             raise t_err
 
-        status = t[0]
-        self.assertEqual(status, errno.GSL_SUCCESS)
-                         
-        val = t[1]
-        err = t[2]
+        return result
 
-        val = float(val)
+
+    def _Check_sf_result(self, orig_args, ref, sf_result):
+        """Check result and error. 
+        
+        Args:
+            * orig_args : arguments the checked function was passed
+            * sf_result : result returned by the function
+            * expected :  expected return value
+            * tolerance : tolerated deviation
+        """
+        test = 0
+        try:
+            val, err  = sf_result
+            test = 1
+        finally:
+            if test == 0:
+                print("sf_result '%s'" %(sf_result,))
+            pass
+
+        test = 0
+        try:
+            expected, tolerance = ref
+            test = 1
+        finally:
+            if test == 0:
+                print("check sf result ref '%s'" %(ref,))
+
+        if type(val) == type(""):
+            val = eval(val)
+        else:
+            val = float(val)
+
+        if type(expected) == type(""):
+            expected = eval(expected)
+        else:
+            expected = float(expected)
+
+        err = self._ConvertError(err)
         err = float(err)
+                
+        fmt = "args=%s, Exp=%s, tol=%s, res =%s+/-%s"
+        msg = fmt %(orig_args, expected, tolerance, val, err)
 
-        msg = "args = '%s'. Exp '%s' tol '%s' res = '%s'+/-'%s'" %(orig_args, result, tolerance, val, err)
+        #print("\n"+ msg)
+        val = float(val)
+        result = float(expected)
+        #print("val", val, type(val))
+        
         if gsl_isnan(val):
             self.assertEqual(np.isfinite(val), False, msg)
             return
@@ -131,30 +227,100 @@ class _test_sf_automatic(unittest.TestCase):
             self.assertEqual(np.isfinite(result), False, msg)
             return
 
-            
-
         self.assertEqual(np.isfinite(val), True, msg)
-
+        
         diff = val - result
         a_diff = np.absolute(diff)
-        tol = tolerance
-
+           
         a_res = np.absolute(result)
-        tol = tolerance * 4 + 1e-5
+
+        tol = self._ConvertTolerance(tolerance)
+        
+        # Check included as extended tests failed?
+        # On the other hand ... sf functions are already testablelel using
+        # gsl tests
+        # If inconsistencies are detedcetd these should be reported to GSL.
+        tol = tol * 4 + 1e-5
         if a_res > 1:
             tol *= a_res
         msg += " diff = %e a_diff = %e tol = %e" %(diff, a_diff, tol)        
         self.assertTrue(a_diff <= tol, msg)
+           
+           
+    def _Check_Func_With_Error(self, orig_args, ref,  ref_status):
+        args = self._GetArgs(orig_args)
 
-    def _Check_Func(self):
-        raise ValueError("not implemented")
+        result, tolerance = ref
+        
+        # If the full func doc is needed for error analysis
+        #func_doc = self._func.__doc__
+        #func_doc = map(lambda x: x.strip(), func_doc.split("\n"))
+        #func_doc = ";".join(func_doc)
+        
+        pygsl.ieee.env_setup()
+
+        t = self._EvalFunc(*args)
+        status = t[0]
+        self.assertEqual(status, ref_status)
+
+        sf_result = t[1:3]
+        self._Check_sf_result(args, ref, sf_result)
     
-    def _test(self, args, result, tolerance, status):
+    def _test(self, orig_args, ref, status):
+        """Test translated from TEST_SF
+
+        Args:
+            orig_args : text stored
+            ref : expected value and error tolerance
+            status : return status
+        """
+        func_name = self._func.__name__
+        if func_name[-2:] == "_e":
+            self._Check_Func_With_Error(orig_args, ref, status)
+        else:            
+            raise  NotImplementedError("Tests for function without status not implemented")
+        
+    def _Check_Func_With_Error2(self, orig_args, ref1, ref2, ref_status):
+        o_args = self._GetArgs(orig_args)
+        pygsl.ieee.env_setup()
+
+        #handle args
+        #print("o_args", o_args)
+        o_args1, o_args2 = o_args
+        
+        call_args = (o_args1,) + (o_args2,)
+        t = self._EvalFunc(*call_args)
+        status = t[0]
+        self.assertEqual(status, ref_status)
+
+        sf_result = t[1:3]        
+        self._Check_sf_result(o_args, ref1, sf_result)
+        
+        sf_result = t[3:5]
+        self._Check_sf_result(o_args, ref2, sf_result)
+
+    def _test2(self, orig_args, ref1, ref2, status):
+        """Test translated from TEST_SF2
+
+        Args:
+            orig_args : text stored
+            ref1 : expected value and error tolerance return from first gsl_result
+            ref1 : expected value and error tolerance return from second gsl_result
+            status : return status
+        """
         if(self._func == None):
+            raise TestNoFuncError("self._func was not defined!")
             return
 
         func_name = self._func.__name__
         if func_name[-2:] == "_e":
-            self._Check_Func_With_Error(args, result, tolerance)
+            test = 0
+            try:
+                self._Check_Func_With_Error2(orig_args, ref1, ref2, status)
+                test = 1
+            finally:
+                if test == 0:
+                    print("orig '%s' ref1 '%s' ref2 '%s' status '%s'" %(orig_args, ref1, ref2, status))
+                
         else:            
-            self._Check_Func()
+            raise  NotImplementedError("Tests for function without status not implemented")
