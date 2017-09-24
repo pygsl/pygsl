eb431fedbf1986b46f5a07ba9ca5cf0ee6c74e96
Pierre Schnizer <Pierre.Schnizer@helmholtz-berlin.de>
2017-09-24 15:13:28 +0200

    put exclude list back; sẃig -xml does not seem to exclude the functions which are labeled ignored in sf.i

--- testing/tools/generate_interface/extract_ufunc_swig.py
+++ testing/tools/generate_interface/extract_ufunc_swig.py
@@ -163,8 +163,76 @@ def create_sf_prototype(f_name, f_params, ret_type):
 
     pass
 
+# Functions to exclude from the whole list.
+exclude_list = ['gsl_sf_angle_restrict_pos_e', # use a double * for input and output. not properly recognized.
+                # use gsl_sf_angle_restricted_pos_err_e complies to the usual interface
+                'gsl_sf_result_smash_e', # the only one to use a const pointer as input. Not recognized by the tool.
+                # Complex Functions are wrapped manually.
+                'gsl_sf_complex_dilog_e',   # polar to rect
+                'gsl_sf_lngamma_complex_e', # rect to polar
+                'gsl_sf_complex_log_e',     # rect to polar
+                'gsl_sf_complex_sin_e',     # rect to rect
+                'gsl_sf_complex_cos_e',     # rect to rect
+                'gsl_sf_complex_logsin_e',
+                #Deprecated functions
+                'gsl_sf_coupling_6j_INCORRECT_e',
+                'gsl_sf_coupling_6j_INCORRECT',
+                # Not a direct map
+                'gsl_sf_polar_to_rect',
+                'gsl_sf_rect_to_polar',
+                # Functions returning arrays are not mapped currently.
+                'gsl_sf_bessel_sequence_Jnu_e',
+                'gsl_sf_bessel_Jn_array',
+                'gsl_sf_bessel_Yn_array',
+                'gsl_sf_bessel_In_array',
+                'gsl_sf_bessel_In_scaled_array',
+                'gsl_sf_bessel_Kn_array',
+                'gsl_sf_bessel_Kn_scaled_array',
+                'gsl_sf_bessel_jl_array',
+                'gsl_sf_bessel_jl_steed_array',
+                'gsl_sf_bessel_yl_array',
+                'gsl_sf_bessel_il_scaled_array',
+                'gsl_sf_bessel_kl_scaled_array',
+                'gsl_sf_coulomb_wave_F_array',
+                'gsl_sf_coulomb_wave_FG_array',
+                'gsl_sf_coulomb_wave_FGp_array',
+                'gsl_sf_coulomb_wave_sphF_array',
+                'gsl_sf_coulomb_CL_array',
+                'gsl_sf_gegenpoly_array',
+                'gsl_sf_legendre_Pl_array',
+                'gsl_sf_legendre_Pl_deriv_array',
+                'gsl_sf_legendre_Plm_array',
+                'gsl_sf_legendre_Plm_deriv_array',
+                'gsl_sf_legendre_sphPlm_array',
+                'gsl_sf_legendre_sphPlm_deriv_array',
+                'gsl_sf_legendre_array_size',
+                'gsl_sf_legendre_H3d_array',
+               'gsl_sf_legendre_array',
+               'gsl_sf_legendre_array_e',
+               'gsl_sf_legendre_deriv_array_e',
+               'gsl_sf_legendre_deriv_array',
+               'gsl_sf_legendre_deriv_alt_array',
+               'gsl_sf_legendre_deriv_alt_array_e',
+               'gsl_sf_legendre_deriv2_array',
+               'gsl_sf_legendre_deriv2_array_e',
+               'gsl_sf_legendre_deriv2_alt_array',
+               'gsl_sf_legendre_deriv2_alt_array_e',
+               'gsl_sf_legendre_array_n',
+               'gsl_sf_legendre_array_index',
+                'gsl_sf_legendre_nlm',
+               'gsl_sf_mathieu_ce_array',
+               'gsl_sf_mathieu_se_array',
+               'gsl_sf_mathieu_Mc_array',
+               'gsl_sf_mathieu_Ms_array',
+                'gsl_sf_mathieu_a_array',
+                'gsl_sf_mathieu_b_array',
+                'gsl_sf_mathieu_workspace',
+                'gsl_sf_mathieu_alloc',
+                'gsl_sf_mathieu_free',
+                "gsl_sf_mathieu_a_coeff",
+                "gsl_sf_mathieu_b_coeff",
+                ]
 
-exclude_list = []
 def extract_func(attr_l, level=None, verbose = None):
     """Get a node and try to build the pattern
 
