7d25a47e29b81459a831844aeaeaa87a28b25e19
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2022-04-26 11:35:41 +0200

    example eigen: increased documentation

--- examples/eigen.py
+++ examples/eigen.py
@@ -1,7 +1,6 @@
 from pygsl import set_debug_level
 import pygsl.eigen as eigen
 import numpy as np
-import sys
 
 # set_debug_level(3)
 
@@ -12,11 +11,12 @@ data = np.array([
  1/4.0, 1/5.0, 1/6.0, 1/7.0])
 
 m = data.reshape(4, 4)
-print(m)
 
-n_evals, A, vec, Z = eigen.eigenvectors_nonsymm(m, compute_t=True, balance=True, schur=True)
-
-
+n_evals, vec, A, Z = eigen.eigenvectors_nonsymm(
+    m, compute_t=True, balance=True, schur=True
+)
+# Be aware: this function works in place
+eigen.eigen_nonsymmv_sort(vec, A, eigen.SortType.abs_desc)
 
 txt = f"""Eigen decomposition result
 
