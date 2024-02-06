2945c5b70ba10bf16d47fac10fcb8db5cf8717f3
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-02-06 19:44:37 +0100

    [FIX] action only on commit to main or pull request to main

--- pygsl/matrix_pierre.py
+++ pygsl/matrix_pierre.py
@@ -1,4 +1,4 @@
-# Author : Pierre Schnizer 
+# Author : Pierre Schnizer
 """
 The matrix module.
 
@@ -35,10 +35,10 @@ Some basic data operations are provided as well (See Section 8.):
     * transpose
 
 And some functions from Section 8.
-    * max          
-    * min          
-    * minmax       
-    * isnull       
+    * max
+    * min
+    * minmax
+    * isnull
     * max_index
     * min_index
     * minmax_index
@@ -48,11 +48,11 @@ from . import block
 
 # Classes allow derivation, contrary to modules. So I wrap the basic type in a
 # generic class here, and use derived classes for the different types of
-# arrays. 
+# arrays.
 class _generic(block._generic_block):
     _base = 'matrix'
     def __init__(self):
-        block._generic_block.__init__(self)
+        block._generic_block.__init__(self) # noqa: F821
         self.diagonal       = self._get_function('diagonal')
         self.subdiagonal    = self._get_function('subdiagonal')
         self.superdiagonal  = self._get_function('superdiagonal')
@@ -61,7 +61,7 @@ class _generic(block._generic_block):
         self.swap_rows      = self._get_function('swap_rows')
         self.swap_rowcol    = self._get_function('swap_rowcol')
         self.transpose      = self._get_function('transpose')
-        
+
 class _complex(_generic):
     _type = 'complex'
 
@@ -89,12 +89,12 @@ class _char(_generic):
 # To allow the access vector.complex.set_all() and the like ...
 complex       = _complex()
 complex_float = _complex_float()
-float         = _float() 
+float         = _float()
 double        = _double()
 long          = _long()
 int           = _int()
 short         = _short()
-char          = _char()            
+char          = _char()
 
 # Remove Helpers from the list
 del block
@@ -102,33 +102,33 @@ del _block
 del _generic
 del _complex
 del _complex_float
-del _float     
+del _float
 del _double
-del _long            
-del _int             
+del _long
+del _int
 del _short
 del _char
 
 # allow to access matrix.double.set_all as matrix.set_all
-set_zero      = double.set_zero      
-set_all       = double.set_all       
-swap          = double.swap          
-isnull        = double.isnull        
-fread         = double._fread        
-fwrite        = double._fwrite       
-fscanf        = double._fscanf       
+set_zero      = double.set_zero
+set_all       = double.set_all
+swap          = double.swap
+isnull        = double.isnull
+fread         = double._fread
+fwrite        = double._fwrite
+fscanf        = double._fscanf
 fprintf       = double._fprintf
-max           = double.max         
-max_index     = double.max_index   
-min           = double.min         
-min_index     = double.min_index   
-minmax        = double.minmax      
+max           = double.max
+max_index     = double.max_index
+min           = double.min
+min_index     = double.min_index
+minmax        = double.minmax
 minmax_index  = double.minmax_index
-diagonal      = double.diagonal     
-subdiagonal   = double.subdiagonal  
+diagonal      = double.diagonal
+subdiagonal   = double.subdiagonal
 superdiagonal = double.superdiagonal
-set_identity  = double.set_identity 
-swap_columns  = double.swap_columns 
-swap_rows     = double.swap_rows    
-swap_rowcol   = double.swap_rowcol  
-transpose     = double.transpose    
+set_identity  = double.set_identity
+swap_columns  = double.swap_columns
+swap_rows     = double.swap_rows
+swap_rowcol   = double.swap_rowcol
+transpose     = double.transpose
