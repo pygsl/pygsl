d3ee79f9c38da5bcaa9c5be7fe36fd730ac61ca2
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-02-18 19:41:47 +0100

    [TASK] pygsl sf: add imported functions to __all__, cleaer code

--- pygsl/testing/sf.py
+++ pygsl/testing/sf.py
@@ -1,20 +1,33 @@
 """Provides the various special functions as UFuncs
 """
+import logging
 from pygsl.testing import _ufuncs
+from pygsl.testing._ufuncs import PREC_DOUBLE, PREC_SINGLE, PREC_APPROX
+
+logger = logging.getLogger("pygsl")
+
+__all__ = ["PREC_DOUBLE", "PREC_SINGLE", "PREC_APPROX"]
+
 _token = "sf_"
 _tokl  = len(_token)
+_shortname = None
+_name = None
+_cmd = None
+
+# remove sf_ from name
 
 for _name in dir(_ufuncs):
     if _name[:_tokl] == _token:
         _shortname = _name[_tokl:]
         _cmd = "%s = _ufuncs.%s" % (_shortname, _name)
-        #printy cmd
+        logger.debug("Adding name %s from %s (module _ufunc)", _shortname, _name)
         exec(_cmd)
-
-from pygsl.testing._ufuncs import PREC_DOUBLE, PREC_SINGLE, PREC_APPROX
+        __all__.append(_shortname)
 
 del _token
 del _tokl
 del _shortname
 del _name
 del _cmd
+
+logger.warning("names listed in __all__: %s", __all__)
