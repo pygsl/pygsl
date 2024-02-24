0b0fcb5ce26a8d26b98bf6be4fdb1534e4c2978d
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-02-24 20:38:37 +0100

    [TASK] rng, examples, debug level to nominal

--- examples/rng.py
+++ examples/rng.py
@@ -21,20 +21,16 @@ k = 5  # number of trials, that means
 # by 'phi' and 'k'
 n = r.multinomial(10, phi, 10)
 print("n = ", n, type(n), n.dtype, n.shape)
-# n = r.multinomial(10, phi)
+n = r.multinomial(3, phi)
 print("n = ", n, type(n), n.dtype, n.shape)
-set_debug_level(0)
 
 # returns an array containing the pdf for each row in 'n'
 
-set_debug_level(0)
-ar =  rng.multinomial_pdf(phi, n)
-print("ar = ", ar, type(ar))
-set_debug_level(0)
+pdf =  rng.multinomial_pdf(phi, n)
+print("pdf = ", pdf)
 
 
 N = 10
 n = r.multinomial(10, phi)
 print("n = ", n)
-set_debug_level(0)
-print(rng.multinomial_lnpdf(a, n))
+print(rng.multinomial_lnpdf(phi, n))
