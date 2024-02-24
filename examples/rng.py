db812e619f0a8842fa041d227cc50ade9a1e26ca
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2024-02-24 20:05:36 +0100

    [TASK] rng example, test: adapted to n_sample

--- examples/rng.py
+++ examples/rng.py
@@ -4,10 +4,13 @@ Example of how to use the rng module
 Here the usage of the multinomoial distribution and its probability function
 is shown.
 """
-from pygsl import rng
+from pygsl import rng, set_debug_level
 import numpy as np
+import sys
 
 r = rng.mt19937_1998()
+
+# multinomial distribution
 phi = (0.2,0.2,0.2,0.4)  # probability distribution over possible events
                          # (is normalized internally if necessary)
 
@@ -16,17 +19,22 @@ k = 5  # number of trials, that means
 
 # sampling 10 data points from the multinomial distribution parametrized
 # by 'phi' and 'k'
-n = r.multinomial(10, phi)
-print("n = ", n, type(n))
+n = r.multinomial(10, phi, 10)
+print("n = ", n, type(n), n.dtype, n.shape)
+# n = r.multinomial(10, phi)
+print("n = ", n, type(n), n.dtype, n.shape)
+set_debug_level(0)
 
 # returns an array containing the pdf for each row in 'n'
-ar =  rng.multinomial_pdf(phi, np.atleast_2d(n))
+
+set_debug_level(0)
+ar =  rng.multinomial_pdf(phi, n)
 print("ar = ", ar, type(ar))
+set_debug_level(0)
 
 
-r = rng.mt19937_1998()
-a = (1,2,3,4,5)
 N = 10
-n = r.multinomial(10, a)
+n = r.multinomial(10, phi)
 print("n = ", n)
-print(rng.multinomial_lnpdf(a, np.atleast_2d(n)))
+set_debug_level(0)
+print(rng.multinomial_lnpdf(a, n))
