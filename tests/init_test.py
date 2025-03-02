0c628de3a7ec6aa74cbc4db96377d9687db5fe23
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-03-02 15:08:07 +0100

    [FIX] updated check func: removed code used for Numeric or numarray

--- tests/init_test.py
+++ tests/init_test.py
@@ -4,10 +4,12 @@ import pygsl.init
 
 def test_init_set_debug_level():
     pygsl.init.set_debug_level(1)
+    pygsl.init.set_debug_level(0)
 
 
 def test_set_debug_level():
     pygsl.set_debug_level(1)
+    pygsl.set_debug_level(0)
 
 
 if __name__ == "__main__":
