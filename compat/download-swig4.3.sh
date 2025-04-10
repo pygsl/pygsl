b9d0fc78b4381e89b04f2ddbc3b1fadaebd6f3c0
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-04-10 20:03:57 +0200

    [FIX] swig version available

--- compat/download-swig4.3.sh
+++ compat/download-swig4.3.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-DSC=http://archive.ubuntu.com/ubuntu/pool/universe/s/swig/swig_4.3.0-0ubuntu2.dsc
+DSC=http://archive.ubuntu.com/ubuntu/pool/universe/s/swig/swig_4.3.0-1.dsc
 ORIG=http://archive.ubuntu.com/ubuntu/pool/universe/s/swig/swig_4.3.0.orig.tar.gz
-DEB_PATCH=http://archive.ubuntu.com/ubuntu/pool/universe/s/swig/swig_4.3.0-0ubuntu2.debian.tar.xz
+DEB_PATCH=http://archive.ubuntu.com/ubuntu/pool/universe/s/swig/swig_4.3.0-1.debian.tar.xz
 
 save_path=$(pwd)
 mkdir -p build/swig4.3
 cd build/swig4.3
 
-curl -O -s $DSC
+curl -O -s $DSC || echo "Failed to download dsc package"
 curl -O -s $ORIG
 curl -O -s $DEB_PATCH
 
