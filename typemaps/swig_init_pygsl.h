e405f1bb0a485be252b05b892e88cf3278b3ecb0
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-11-01 09:07:01 +0100

    [TASK] gsl sf function, unfuncs generator: reduce verbosity of wrapper generator

--- typemaps/swig_init_pygsl.h
+++ typemaps/swig_init_pygsl.h
@@ -4,9 +4,10 @@
 
 #if SWIG_VERSION >= 0x040400 // SWIG >= 4.4.0
 #define swig_init_pygsl() init_pygsl1(-1)
+#define swig_import_pygsl_rng() import_pygsl_rng1(-1)
 #define swig_init_test_fail() do{ return -1; }while(0)
 #else
-#define swig_init_pygsl() init_pygsl1(NULL)
+#define swig_import_pygsl_rng() import_pygsl_rng()
 #define swig_init_test_fail() do{ return NULL; }while(0)
 #endif
 
