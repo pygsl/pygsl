b7295f6073eed5b80fc83e99c5bff6f242cabfd3
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2025-11-01 08:44:22 +0100

    [TASK] swig_init_pygsl handles swig return for different swig version for pygsl init

---
+++ tests/swig_import_test.py
@@ -0,0 +1,17 @@
+"""Check that swig_pygsl_init returns the flag which triggers an import error on failure
+"""
+import pytest
+
+
+def test_swig_init_returns_correct_flag():
+    """
+    Before version 4.4 it would check that a pointer is returned. So
+    error was signaled with NULL. Since 4.4 this means success, and -1
+    needs to be returned to signal failure
+
+    Please write me a email (or even better open a pull request)
+    if this assumption is wrong
+    """
+    with pytest.raises(ImportError):
+        from pygsl import _swig_fail_on_init
+        from pygsl import swig_fail_on_init
